81bc97f137752a1e9f64e396abdd711d39769b92
Sangwon Ryu <bdboy090@naver.com>
2020-03-01 22:07:10 +0700

    파일 읽기 구현

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::process;
+use std::env;
+use std::fs;
+
 use nokheui::hangul::disassemble;
 
 fn main() {
-    println!("{:?}", disassemble('뷁'));    
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("usage: ./nokheui [options] filename");
+        process::exit(-1);
+    }
+
+    let data: String = fs::read_to_string(&args[1]).unwrap();
+
+    println!("{:?}", data);    
 }
