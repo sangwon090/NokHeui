156047a8399e93104720d13f64af8e500b45be71
Sangwon Ryu <bdboy090@naver.com>
2020-03-01 23:59:04 +0700

    커서 추가

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::collections::VecDeque;
 #[derive(Debug)]
 pub struct Nokheui {
     code: Vec<Vec<char>>,
+    cursor: (u32, u32),
     stacks: [Vec<i32>; 26],
     queue: VecDeque<i32>
 }
@@ -13,8 +14,9 @@ impl Nokheui {
     pub fn new(code: Vec<Vec<char>>) -> Nokheui {
         Nokheui {
             code: code,
+            cursor: (0, 0),
             stacks: Default::default(),
-            queue: Default::default()
+            queue: Default::default(),
         }
     }
 
