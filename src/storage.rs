efbe8cf79dad7ef4677846415240925fdcd0f1f7
Sangwon Ryu <bdboy090@naver.com>
2020-03-05 20:04:40 +0700

    변수의 이름을 일관성 있게 바꿈

--- src/storage.rs
+++ src/storage.rs
@@ -16,28 +16,28 @@ impl Storage {
         }
     }
 
-    pub fn push(&mut self, memory_number: usize, value: i32) {        
-        if memory_number <= 25 {
-            self.stacks[memory_number].push(value);
-        } else if memory_number == 26 {
+    pub fn push(&mut self, storage_number: usize, value: i32) {        
+        if storage_number <= 25 {
+            self.stacks[storage_number].push(value);
+        } else if storage_number == 26 {
             self.queue.push_back(value);
-        } else if memory_number == 27 {
+        } else if storage_number == 27 {
             eprintln!("[*] Pipe is not implemented.")
         } else {
-            eprintln!("[*] #{} is invalid storage.", memory_number);
+            eprintln!("[*] #{} is invalid storage.", storage_number);
         }
     }
 
-    pub fn pop(&mut self, memory_number: usize) -> i32 {
-        if memory_number <= 25 {
-            match self.stacks[memory_number].pop() {
+    pub fn pop(&mut self, storage_number: usize) -> i32 {
+        if storage_number <= 25 {
+            match self.stacks[storage_number].pop() {
                 Some(n) => return n,
                 None => {
-                    eprintln!("[*] Stack {} is empty.", memory_number);
+                    eprintln!("[*] Stack {} is empty.", storage_number);
                     return 0;
                 }
             }
-        } else if memory_number == 26 {
+        } else if storage_number == 26 {
             match self.queue.pop_front() {
                 Some(n) => return n,
                 None => {
@@ -45,37 +45,37 @@ impl Storage {
                     return 0;
                 }
             }
-        } else if memory_number == 27 {
+        } else if storage_number == 27 {
             eprintln!("[*] Pipe is not implemented.");
             return 0;
         } else {
-            eprintln!("[*] {} is invalid storage number.", memory_number);
+            eprintln!("[*] {} is invalid storage number.", storage_number);
             return 0;
         } 
     }
 
-    pub fn duplicate(&mut self, memory_number: usize) {        
-        if memory_number <= 25 {
-            let value: i32 = self.stacks[memory_number].last().cloned().unwrap();
+    pub fn duplicate(&mut self, storage_number: usize) {        
+        if storage_number <= 25 {
+            let value: i32 = self.stacks[storage_number].last().cloned().unwrap();
 
-            self.stacks[memory_number].push(value);
-        } else if memory_number == 26 {
+            self.stacks[storage_number].push(value);
+        } else if storage_number == 26 {
             let value: i32 = self.queue[0];
 
             self.queue.push_front(value);
-        } else if memory_number == 27 {
+        } else if storage_number == 27 {
             eprintln!("[*] Pipe is not implemented.")
         } else {
-            eprintln!("[*] #{} is invalid storage.", memory_number);
+            eprintln!("[*] #{} is invalid storage.", storage_number);
         }
     }
 
-    pub fn swap(&mut self, memory_number: usize) {
-        if memory_number <= 25 {
-            let length: usize = self.stacks[memory_number].len();
+    pub fn swap(&mut self, storage_number: usize) {
+        if storage_number <= 25 {
+            let length: usize = self.stacks[storage_number].len();
 
-            self.stacks[memory_number].swap(length - 1, length - 2);
-        } else if memory_number == 26 {
+            self.stacks[storage_number].swap(length - 1, length - 2);
+        } else if storage_number == 26 {
             let a: i32 = match self.queue.pop_front() {
                 Some(n) => n,
                 None => {
@@ -94,19 +94,19 @@ impl Storage {
 
             self.queue.push_front(a);
             self.queue.push_front(b);
-        } else if memory_number == 27 {
+        } else if storage_number == 27 {
             eprintln!("[*] Pipe is not implemented.")
         } else {
-            eprintln!("[*] #{} is invalid storage.", memory_number);
+            eprintln!("[*] #{} is invalid storage.", storage_number);
         }
     }
 }
 
-pub fn get_memory_number(memory_name: char) -> usize {
-    match JONGSEONG_LIST.iter().position(|&c| c == memory_name) {
+pub fn get_memory_number(storage_name: char) -> usize {
+    match JONGSEONG_LIST.iter().position(|&c| c == storage_name) {
         Some(n) => return n,
         None => {
-            eprintln!("[*] {} is invalid storage number.", memory_name);
+            eprintln!("[*] {} is invalid storage number.", storage_name);
             return 0;
         }
     }
